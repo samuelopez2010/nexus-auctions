79b72301767c31cd36f93040b714fedc6c616094
samuelopez2010 <samuelopez2010@gmail.com>
2026-02-01 19:01:02 -0400

    fix: Serve media files in production

--- nexus_core/urls.py
+++ nexus_core/urls.py
@@ -61,5 +61,9 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    from django.views.static import serve
+    from django.urls import re_path
+
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
